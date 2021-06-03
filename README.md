7afed85c7f6c29cde59168a0d1faf47d4d9c6450
jackjona123 <65679392+jackjona123@users.noreply.github.com>
2021-06-03 18:48:00 -0400

    production

---
+++ README.md
@@ -0,0 +1,23 @@
+# Jack Jona ReactJS Portfolio Website
+
+## Description
+This is a ReactJS based personal resume/portfolio single page website.
+
+## Portfolio Sections
+✔️ Summary and About me
+✔️ Skills
+✔️ Education
+✔️ Work Experience
+✔️ Open Source Projects Connected with Github
+✔️ Big Projects
+✔️ Achievements And Certifications 
+✔️ Blogs
+✔️ Talks
+✔️ Podcast
+✔️ Contact me
+✔️ Twitter Timeline
+✔️ Github Profile
+
+## Credits
+
+#### Credits to Saad Pasta <a href='https://github.com/saadpasta/developerFolio'>Original Repo</a>
---
+++ src/portfolio.js
@@ -0,0 +1,372 @@
+﻿/* Change this file to get your personal Portfolio */
+
+// Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const illustration = {
+  animated: true // set to false to use static SVG
+};
+
+const greeting = {
+  username: "Jack Jona",
+  title: "Hello Everyone, I'm Jack Jona!",
+  subTitle: emoji(
+    "I'm a optimistic and passionate Full Stack Software Developer with experience in designing and developing Web and Mobile applications.  I always enjoy a good challengeand continue to learn from my mistakes."
+  ),
+  resumeLink:
+    "https://github.com/jackjona123",
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/jackjona123",
+  instagram: 'https://github.com/jackjona123?tab=repositories',
+  twitter: 'https://twitter.com/jackjona123',
+  mail: "jackjona123@pm.me",
+  // linkedin: "https://www.linkedin.com/in/",
+  // gitlab: "https://gitlab.com/",
+  // facebook: "https://www.facebook.com/"
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "A PASSIONATE FULL STACK DEVELOPER WHO WANTS TO EXPLORE AND KEEP ON LEARNING",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA )"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "swift",
+      fontAwesomeClassname: "fab fa-swift"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "fab fa-git"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+      skillName: "gcp",
+      fontAwesomeClassname: "fab fa-google"
+    },
+    {
+      skillName: "linux",
+      fontAwesomeClassname: "fab fa-linux"
+    },
+    {
+      skillName: "windows",
+      fontAwesomeClassname: "fab fa-windows"
+    },
+    {
+      skillName: "macos",
+      fontAwesomeClassname: "fab fa-apple"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: false, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Harvard University",
+      logo: require("./assets/images/harvardLogo.png"),
+      subHeader: "Master of Science in Computer Science",
+      duration: "September 2017 - April 2019",
+      desc: "Participated in the research of XXX and published 3 papers.",
+      descBullets: [
+        "Description Bullets Here"
+      ]
+    },
+    {
+      schoolName: "Stanford University",
+      logo: require("./assets/images/stanfordLogo.png"),
+      subHeader: "Bachelor of Science in Computer Science",
+      duration: "September 2013 - April 2017",
+      desc:
+        "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+      descBullets: ["Description Bullets Here"]
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend / Design", //Insert stack or technology you have experience in
+      progressPercentage: "95%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "90%"
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%"
+    }
+  ],
+  displayCodersrank: true // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: false, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Front-End Web Developer",
+      company: "Jack Solutions",
+      companylogo: require("./assets/images/jacksolutions.png"),
+      date: "September 2020 – Present",
+      desc:
+        "",
+      descBullets: [
+        "Custom building web applications",
+        "Designing UI's",
+      ]
+    },
+    {
+      role: "Systems Administrator (SysAdmin)",
+      company: "Jack Technology",
+      companylogo: require("./assets/images/jacktech.png"),
+      date: "November 2018 – July 2019",
+      desc:
+        ""
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: "jackjona123", // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "SOME Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED",
+  projects: [
+    {
+      image: require("./assets/images/jacktech.png"),
+      projectName: "Jack Technology",
+      projectDesc: "Designed & Developed Thier Website",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://jacktech.cf"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/jacksolutions.png"),
+      projectName: "Jack Solutions",
+      projectDesc: "Designed & Developed Thier Website",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://jacksolutions.cf"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/cloudhack.png"),
+      projectName: "CloudHack Technologies",
+      projectDesc: "Designed & Developed Thier Website",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://cloudhacktech.cf"
+        }
+      ]
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      footerLink: [
+        {name: "Certification", url: ""},
+        {
+          name: "Final Project",
+          url: ""
+        }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+        "Developed a Google Assistant Action that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url:
+            ""
+        }
+      ]
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blog",
+  subtitle:
+    " ",
+
+  blogs: [
+    {
+      url: "https://blog.jackjona.live/",
+      title: "Click Here To View My Blog",
+      description:
+        "📝 :) "
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    ""
+  ),
+
+  talks: [
+    {
+      title: "",
+      subtitle: "",
+      slides_url: "",
+      event_url: ""
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast"),
+  subtitle: "PODCAST INDO",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    ""
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ✉️"),
+  subtitle:
+    "Want to discuss a project or just want to say hi? Get in touch!",
+  number: "(647) 499-4219",
+  email_address: "jackjona123@pm.me"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "jackjona123", //Replace "twitter" with your twitter username without @
+  display: true // Set true to display this section, defaults to false
+};
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails
+};
