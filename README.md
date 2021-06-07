be2f5bb87415992a716ab0d7f87810a1c24ed11d
jackjona123 <65679392+jackjona123@users.noreply.github.com>
2021-06-07 16:16:36 -0400

    production

---
+++ README.md
@@ -0,0 +1,42 @@
+# Jack Jona React Portfolio/Website v2
+
+<p align="center">
+  <a href="https://reactjs.org/"><img alt="Made With React" src="https://img.shields.io/badge/made%20with-react-61DAFB?style=flat-square" /></a>
+  <a href="https://github.com/prettier/prettier"><img alt="code style: prettier" src="https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square?style=flat-square" /></a>
+  <br/>
+  <a href="http://badges.mit-license.org/"><img alt="License" src="http://img.shields.io/:license-mit-blue.svg?style=flat-square?style=flat-square" /></a>
+  <a href="http://badges.mit-license.org/"><img alt="Website" src="https://img.shields.io/badge/website-up-yellow?style=flat-square" /></a>
+
+## Live Site
+
+To view the live website, **[click here](https://v2.jackjona.live/)**
+
+
+## Sections 📚
+
+✔️ Summary and About me\
+✔️ Skills \
+✔️ Open Source Projects Connected with Github\
+✔️ Experience\
+✔️ Certifications 🏆\
+✔️ Blogs\
+✔️ Education\
+✔️ Contact me
+
+
+## Technologies used 🛠️
+
+- [React](https://reactjs.org/)
+- [graphql](https://graphql.org/)
+- [apollo-boost](https://www.apollographql.com/docs/react/get-started/)
+- [baseui](https://github.com/uber/baseweb)
+- [react-reveal](https://www.react-reveal.com/)
+- [styled-components](https://styled-components.com/)
+
+### illustrations 🍥
+
+- [UnDraw](https://undraw.co/illustrations)
+
+### License 📄
+
+This project is licensed under the MIT License - see the [LICENSE.md](./LICENSE) file for details.
---
+++ src/portfolio.js
@@ -0,0 +1,314 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to false if you don't want Splash screen.
+};
+
+//SEO Related settings
+const seo = {
+  title: "Jack Jona's Portfolio",
+  description:
+    "I'm a passionate Full Stack Software Developer with experience in designing and developing Web and Mobile applications who want's to keep on learning. I always enjoy a good challenge and continue to learn from my mistakes.",
+  og: {
+    title: "Jack Jona's Portfolio",
+    type: "website",
+    url: "https://alt.jackjona.live",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Jack Jona",
+  logo_name: "Jack Jona",
+  nickname: "jackjona123",
+  subTitle:
+    "I'm a passionate Full Stack Software Developer with experience in designing and developing Web and Mobile applications who want's to keep on learning. I always enjoy a good challenge and continue to learn from my mistakes.",
+  resumeLink:
+    "https://jackjona.ga",
+  portfolio_repository: "https://github.com/jackjona123/jackjona123",
+};
+
+const socialMediaLinks = [
+  /* Your Social Media Link */
+  // github: "https://github.com/jackjona123",
+  // linkedin: "https://www.linkedin.com/in/jackjona123/",
+  // gmail: "jackjona123@pm.me",
+  // gitlab: "https://gitlab.com/jackjona",
+  // facebook: "https://www.facebook.com/jackjona123/",
+  // twitter: "https://twitter.com/jackjona123",
+  // instagram: "https://www.instagram.com/jackjona123/"
+
+  {
+    name: "Github",
+    link: "https://github.com/jackjona123",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "mail",
+    link: "mailto:jackjona123@pm.me",
+    fontAwesomeIcon: "fas fa-envelope", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#000000", // Reference https://simpleicons.org/?q=gmail
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/jackjona123",
+    fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
+    backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/jackjona123/",
+    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  },
+];
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Developing scalable production ready website applications",
+        "⚡ Building resposive website front end using React-Redux",
+        "⚡ Developing mobile applications using Flutter, React Native and solo android apps using Kotlin",
+        "⚡ Creating application backend in Node, Express & Flask",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "Sass",
+          fontAwesomeClassname: "simple-icons:sass",
+          style: {
+            color: "#CC6699",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS & React Native",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "Yarn",
+          fontAwesomeClassname: "simple-icons:yarn",
+          style: {
+            color: "#2C8EBB",
+          },
+        },
+        {
+          skillName: "Gatsby",
+          fontAwesomeClassname: "simple-icons:gatsby",
+          style: {
+            color: "#663399",
+          },
+        },
+        {
+          skillName: "Next-Js",
+          fontAwesomeClassname: "simple-icons:nextdotjs",
+          style: {
+            color: "#000000",
+          },
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases",
+        "⚡ Deploying applications to the cloud for easy access and maintenance",
+        "⚡ Setting up streaming jobs from DB to Server or vice-versa on GCP and AWS",
+      ],
+      softwareSkills: [
+        {
+          skillName: "GCP",
+          fontAwesomeClassname: "simple-icons:googlecloud",
+          style: {
+            color: "#4285F4",
+          },
+        },
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Azure",
+          fontAwesomeClassname: "simple-icons:microsoftazure",
+          style: {
+            color: "#0089D6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "Kubernetes",
+          fontAwesomeClassname: "simple-icons:kubernetes",
+          style: {
+            color: "#326CE5",
+          },
+        },
+      ],
+    },
+    {
+      title: "UI/UX Design",
+      fileName: "DesignImg",
+      skills: [
+        "⚡ Designing highly attractive user interface for mobile and web applications",
+        "⚡ Customizing logo designs and building logos from scratch",
+        "⚡ Creating the flow of application functionalities to optimize user experience",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Adobe XD",
+          fontAwesomeClassname: "simple-icons:adobexd",
+          style: {
+            color: "#FF2BC2",
+          },
+        },
+        {
+          skillName: "Figma",
+          fontAwesomeClassname: "simple-icons:figma",
+          style: {
+            color: "#F24E1E",
+          },
+        },
+        {
+          skillName: "Adobe Illustrator",
+          fontAwesomeClassname: "simple-icons:adobeillustrator",
+          style: {
+            color: "#FF7C00",
+          },
+        },
+        {
+          skillName: "Inkscape",
+          fontAwesomeClassname: "simple-icons:inkscape",
+          style: {
+            color: "#000000",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects utilize the various latest technology tools. I create web and mobile deploy them using cloud infrastructure.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me 👋",
+    profile_image_path: "me.png",
+    description:
+      "Want to discuss a project or just want to say hi? Get in touch!",
+  },
+  blogSection: {
+    title: "Blog",
+    subtitle:
+      "For when I want to write something and put it out on the internet :)",
+    link: "https://blog.jackjona.live",
+    avatar_image_path: "blogs_image.svg",
+  },
+  addressSection: {
+    title: "Email",
+    subtitle:
+      "jackjona123@pm.me",
+    avatar_image_path: "address_image.svg",
+    location_map_link: "mailto:jackjona123@pm.me",
+  },
+  phoneSection: {
+    title: "Phone Number",
+    subtitle: "(647) 499-4219",
+  },
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+  projectsHeader,
+  contactPageData,
+};
